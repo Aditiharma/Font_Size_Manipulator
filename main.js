ddda653de11988d21ac9a982d5acfee407c12db5
Aditiharma <79885326+Aditiharma@users.noreply.github.com>
2022-02-08 19:05:54 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw(){
+    background('cyan');
+    text('Aditi', 50, 400);
+    fill("blue");
+    document.getElementById("square_sides").innerHTML = "The font size ofthe text will be = " + difference + "px";
+    textSize(difference);
+}
+
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+function gotPoses(results){
+    if(results.length > 0 ){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+      }
+}
